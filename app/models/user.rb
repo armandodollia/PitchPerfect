fb9fea19bbbcdc2145b44ab8a860059f1bac007d
Armando Dollia <dollia.armando@gmail.com>
2017-03-29 15:42:40 -0500

    Completes user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :votes, foreign_key: :voter_id
+  has_many :pitches, foreign_key: :author_id
+  belongs_to :team, class_name: 'Pitch'
 end
