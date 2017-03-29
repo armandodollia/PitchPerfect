819240054760326678a18cfe4bbedfd6efcb9e14
Mike Bonetti & Ken Saganski <pair+mikeyb303.saganawski@devbootcamp.com>
2017-03-29 17:56:53 -0500

    add log in to the home page, error test and vaildates user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to 'days#index'
+    else
+      @errors = ["Invalid email/password combination"]
+      render 'users/new'
+    end
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root 'users#new'
   resources :users, only: [:new, :create]
   resources :days, only: [:index]
+  resources :sessions, only: [:create, :destroy]
 end
