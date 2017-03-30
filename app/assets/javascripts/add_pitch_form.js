e76952a39bd458e7dfbc3f1f3cc7ec60ca312942
Armando Dollia <dollia.armando@gmail.com>
2017-03-30 15:21:50 -0500

    Implements ajax for adding new pitch

--- app/assets/javascripts/add_pitch_form.js
+++ app/assets/javascripts/add_pitch_form.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
-  $('#show_pitch_form').on('click', function (event) {
+  $('#show_pitch_form_button').on('click', function (event) {
     event.preventDefault();
-    $('#new_pitch_form').removeClass('hide');
+    $('#add_pitch_form_container').removeClass('hide');
     $(this).addClass('hide');
 
   });
 
-  $('#new_pitch_form').on('submit', '.btn', function (event) {
-    // event.preventDefault();
-    $('#new_pitch_form').addClass('hide');
-    $('#show_pitch_form').removeClass('hide');
+  $('#new_pitch').on('submit', '.btn', function (event) {
+    var $form = $(this)
+    event.preventDefault();
+
+
+    var call = $form.click(function(event){
+      event.preventDefault()
+      $.ajax({
+        url: $form.attr('action'),
+        method: $form.attr('method'),
+        data: $form.serialize()
+      });
+
+    });
   });
 });
--- app/controllers/pitches_controller.rb
+++ app/controllers/pitches_controller.rb
@@ -3,18 +3,19 @@ class PitchesController < ApplicationController
   def create
     params[:pitch][:author_id] = current_user.id
     params[:pitch][:day_id] = params[:day_id]
-    # p pitches_params
-    # p params
-    # puts "* " * 30
-    pitch = Pitch.new(pitches_params)
+    @pitch = Pitch.new(pitches_params)
     
-    if pitch.save
-      # puts "* " * 30
-      # p pitch
-      redirect_to "/days/#{params[:day_id]}"
+    respond_to do |format|
+      if @pitch.save
+        format.html {
+          redirect_to "/days/#{params[:day_id]}"
+        }
+        format.js { render :layout => false }
+      
     else
       
       
+      end
     end
   end
 
