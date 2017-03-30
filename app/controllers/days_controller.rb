358c8fa41614190f7f30b722c6d774ef9e5a4a75
Ken Saganski <saganawski@gmail.com>
2017-03-30 16:38:39 -0500

    admin can add a new day, admin will see errors if submision invailid

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -11,7 +11,22 @@ class DaysController < ApplicationController
   end
 
   def create
+  	p params
+  	puts "*" * 30
+  	day = Day.new(days_params)
+  	if day.save
+  		redirect_to days_path
+  	else
+  		@errors = day.errors.full_messages
+  		render new_day_path
+  	end
+
   end
+
+  private
+	def days_params
+		params.require(:day).permit(:cohort_name, :pic_url, :round_status, :date, :passing_number, :teams_total)
+	end
 
 end
 
