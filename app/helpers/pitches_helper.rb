6f000a99293c830e0b577295182eb7eb8e3512b5
Chase Stephens & Mike Bonetti <pair+chastep.mikeyb303@devbootcamp.com>
2017-03-30 11:15:41 -0500

    Add sorting and winner methods

--- app/helpers/pitches_helper.rb
+++ app/helpers/pitches_helper.rb
@@ -1,8 +1,12 @@
 module PitchesHelper
-  def round_1_winners
-    # p "--------------------------------------"
-    # @pitches = Pitch.all.where(day_id: @day.id).order(:votes)
-    # @votes = @pitches.map { |pitch| pitch.votes }
-    # p @votes.order(points: :desc)
+  def sort_by_points_total(pitches)
+    points_hash = Hash.new
+    pitches.each_with_index { |pitch, index| points_hash[index] = pitch.points_total }
+    sorted_hash = Hash[points_hash.sort_by { |id, points| points }.reverse]
+  end
+
+  def round_1_winners(pitches, day)
+    sorted_pitches = sort_by_points_total(pitches)
+    winners = Hash[sorted_pitches.sort_by {|id, points| -points}[0...(day.passing_number)]]
   end
 end
