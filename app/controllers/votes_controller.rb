dbec81e8b395bf93d3f47f8a5f25dde3083967ce
Chase Stephens & Mike Bonetti <pair+chastep.mikeyb303@devbootcamp.com>
2017-03-30 17:29:46 -0500

    Users can now vote on pitches

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,31 +1,25 @@
 class VotesController < ApplicationController
   def create
-    # p "-------------------------------------"
-    # p params
     params[:vote] = Hash.new
-    params[:vote][:voter_id] = current_user.id
-    params[:vote][:day_id] = params[:day_id]
     day = Day.find_by(id: params[:day_id])
+    if day.round_status == 'round_1'
+      current_round = 1
+    elsif day.round_status == 'round_2'
+      current_round = 2
+    end
+    params[:vote][:voter_id] = current_user.id
     pitch_count = day.pitches.count
-    # p params
     params.each do |key, value|
-      # p "------------------------------"
-      # p key
-      # p key.class
-      # p key.to_s.to_i
-      # p "------------------------------"
       if key.to_s.to_i > 0
-        # p key.class
         params[:vote][:pitch_id] = key
         params[:vote][:points] = pitch_count - value.to_s.to_i
-        p vote_params
-        # Vote.create!(vote_params)
+        day.rounds.find_by(round_number: current_round).votes.new(vote_params).save
       end
     end
   end
 
   private
     def vote_params
-      params.require(:vote).permit(:pitch_id, :voter_id, :day_id, :points)
+      params.require(:vote).permit(:pitch_id, :voter_id, :points)
     end
 end
