520bb385dc4031df9800049db0d38151557d8c21
Ken Saganski <saganawski@gmail.com>
2017-03-30 09:59:05 -0500

    Tweak the routing for login and register, that it routes to the new days index page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to 'days#index'
+      redirect_to '/days#index'
     else
       @errors = ["Invalid email/password combination"]
       render '/users/new'
