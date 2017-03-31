46c27d29b79ce7570c1c002fc6d313416c159854
Chase Stephens & Mike Bonetti <pair+chastep.mikeyb303@devbootcamp.com>
2017-03-30 22:10:23 -0500

    fix home logo link

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,4 @@
 module UsersHelper
-  # def voted?(user, pitches, round_number)
-  #   pitches.each do |pitch|
-  #     pitch.votes.each do |vote|
-  #       if vote.voter_id == user.id && vote.round.round_number == round_number
-  #         return true
-  #       end
-  #     end
-  #   end
-  #   false
-  # end
 
   def voted?(day, round_number)
     p day.rounds.find_by(round_number: round_number).votes
@@ -23,11 +13,3 @@ module UsersHelper
 
 end
 
-# def voted?(day)
-#   day.rounds.find(round_number).votes.each do |vote|
-#     if vote.voter_id == current_user
-#       return true
-#     end
-#     false
-#   end
-# end
