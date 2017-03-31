d00654970ce8d7c86b3b4c4a128c082ceca13840
Armando Dollia <dollia.armando@gmail.com>
2017-03-30 21:55:08 -0500

    Fixes radio button

--- app/assets/javascripts/radioButtons.js
+++ app/assets/javascripts/radioButtons.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  $("body").on('click', "input[type=radio]", function(event) {
-    debugger
+  $('.pitch-list').on('click', "input[type=radio]", function() {
     var el = $(this);
     var col = el.data("col");
     $("input[data-col=" + col + "]").prop("checked", false);
